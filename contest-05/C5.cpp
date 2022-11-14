72eb16b28c0e813c19d1021b482ad42a9a29218f
elkcl <28754537+elkcl@users.noreply.github.com>
2022-11-14 10:11:32 +0300

    ЗАКРЫЛ КОРНЯЧКУ!!!!!!

--- contest-05/C5.cpp
+++ contest-05/C5.cpp
@@ -21,6 +21,7 @@
 #include <deque>
 #include <stack>
 #include <unordered_set>
+#include <bitset>
 
 //#define int int64_t
 
@@ -57,6 +58,7 @@ ostream& operator<<(ostream& s, const vector<T>& v) {
 }
 
 const int BLOCK = 300;
+const int MAXN = 100001;
 
 signed main() {
     ios_base::sync_with_stdio(false);
@@ -64,7 +66,79 @@ signed main() {
 
     int n, m, q;
     cin >> n >> m >> q;
-    vi a(n);
+    vll a(n);
     cin >> a;
+    int bls = 0;
+    vi heavy_id(m, -1);
+    vector<bitset<MAXN>> heavy_contains;
+    vll heavy_base;
+    vll heavy_add;
+    vvi sets(m);
+    for (int i = 0; i < m; ++i) {
+        int k;
+        cin >> k;
+        sets[i].resize(k);
+        if (k > BLOCK) {
+            heavy_id[i] = bls;
+            heavy_contains.emplace_back();
+            heavy_base.push_back(0);
+            heavy_add.push_back(0);
+            for (int j = 0; j < k; ++j) {
+                cin >> sets[i][j];
+                --sets[i][j];
+                heavy_contains[bls][sets[i][j]] = true;
+                heavy_base[bls] += a[sets[i][j]];
+            }
+            ++bls;
+        } else {
+            for (int j = 0; j < k; ++j) {
+                cin >> sets[i][j];
+                --sets[i][j];
+            }
+        }
+    }
+    vvi intersect(bls, vi(m, 0));
+    for (int i = 0; i < bls; ++i) {
+        for (int j = 0; j < m; ++j) {
+            for (int e : sets[j]) {
+                if (heavy_contains[i][e])
+                    ++intersect[i][j];
+            }
+        }
+    }
 
+    while (q--) {
+        char t;
+        cin >> t;
+        if (t == '+') {
+            int k, x;
+            cin >> k >> x;
+            --k;
+            if (heavy_id[k] == -1) {
+                for (int e : sets[k])
+                    a[e] += x;
+                for (int i = 0; i < bls; ++i)
+                    heavy_base[i] += 1ll * x * intersect[i][k];
+            } else {
+                heavy_add[heavy_id[k]] += x;
+            }
+        } else {
+            int k;
+            cin >> k;
+            --k;
+            if (heavy_id[k] == -1) {
+                ll ans = 0;
+                for (int e : sets[k])
+                    ans += a[e];
+                for (int i = 0; i < bls; ++i)
+                    ans += 1ll * heavy_add[i] * intersect[i][k];
+                cout << ans << nl;
+            } else {
+                ll ans = heavy_base[heavy_id[k]];
+                for (int i = 0; i < bls; ++i)
+                    ans += 1ll * heavy_add[i] * intersect[i][k];
+                cout << ans << nl;
+            }
+        }
+    }
 }
